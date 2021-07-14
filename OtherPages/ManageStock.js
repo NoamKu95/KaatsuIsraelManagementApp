75b1a159bcd82aec7c1479bfccfb85901f0b8769
Noam Kurtzer <68505926+NoamKu95@users.noreply.github.com>
2021-07-14 20:48:25 +0300

    added let a

--- OtherPages/ManageStock.js
+++ OtherPages/ManageStock.js
@@ -193,6 +193,7 @@ export default function ManageStock(props) {
   //Send the edited products to the DB:
   const sendNewAmountsOfProducts = () => {
 
+    let a = 0;
     const apiUrl = `https://proj.ruppin.ac.il/bgroup12/prod/api/editstock`;
     fetch(apiUrl,
       {
